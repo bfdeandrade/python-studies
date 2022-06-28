f53a4637f4e095addfe57e7ab382aa81eee340dc
Breno Andrade <bfdeandrade@gmail.com>
2022-06-28 11:46:32 -0300

    Adicionado requests para randomizar questions

--- day-17/quiz-game-start/main.py
+++ day-17/quiz-game-start/main.py
@@ -1,11 +1,10 @@
-import imp
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 question_bank = list()
 for i in question_data:
-    current_text = i['text']
-    current_answer = i['answer']
+    current_text = i['question']
+    current_answer = i['correct_answer']
     question_bank.append(Question(current_text, current_answer))
     
 
