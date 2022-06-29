754950d2a05e672715efafe006c7b794a1e71295
Breno Andrade <bfdeandrade@gmail.com>
2022-06-29 12:19:49 -0300

    Feature: segunda parte Desafio concluido

--- day-18/hirstpaiting/main.py
+++ day-18/hirstpaiting/main.py
@@ -1,6 +1,7 @@
 from curses import color_content
-from turtle import color
+from turtle import Turtle, Screen
 import colorgram
+from random import choice
 
 
 colors_list = list()
@@ -11,4 +12,21 @@ for color in colors:
     r, g, b = rgb.r, rgb.g, rgb.b
     tuple_rgb  = (r, g, b)
     colors_list.append(tuple_rgb)
-print(colors_list)
+
+
+tim = Turtle()
+tim.shape("arrow")
+screen = Screen()
+screen.colormode(255)
+def draw_dots(size_of_dot, size_of_gap, color_list):
+    for pos in range(0, 500, 50):
+        tim.setposition(0,pos)
+        for i in range(10):
+            tim.dot(size_of_dot, choice(colors_list))
+            tim.pu()
+            tim.forward(50)
+
+
+draw_dots(10, 50, colors_list)
+
+screen.exitonclick()
